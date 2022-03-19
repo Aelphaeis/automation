b97b3688b95a41e09260aaba44517159df125a86
Joseph <aelphaeis@live.com>
2022-03-19 18:16:29 -0400

    initial commit

---
+++ src/test/java/com/cruat/automation/ProgramTest.java
@@ -0,0 +1,12 @@
+package com.cruat.automation;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Test;
+
+class ProgramTest {
+
+	@Test
+	void testMain() {
+		assertTrue(true);
+	}
+}
