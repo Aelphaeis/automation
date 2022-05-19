ea085d154effa5b869c82182deed0a44a52156e2
Joseph <aelphaeis@live.com>
2022-05-19 17:40:06 -0400

    Add Thread utility.

---
+++ src/main/java/com/cruat/automation/utilities/Threads.java
@@ -0,0 +1,17 @@
+package com.cruat.automation.utilities;
+
+public class Threads {
+
+	public static void pause(long milliseconds) {
+		try {
+			Thread.sleep(milliseconds);
+		}
+		catch (InterruptedException e) {
+			Thread.currentThread().interrupt();
+		}
+	}
+
+	private Threads() {
+		throw new UnsupportedOperationException("utility class");
+	}
+}
