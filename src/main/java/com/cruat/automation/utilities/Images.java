c044c775e406199aa5371a5d8c7847dcb290be15
Joseph <aelphaeis@live.com>
2022-05-20 16:15:55 -0400

    Add Copy image functionality.

--- src/main/java/com/cruat/automation/utilities/Images.java
+++ src/main/java/com/cruat/automation/utilities/Images.java
@@ -2,6 +2,8 @@ package com.cruat.automation.utilities;
 
 import java.awt.FlowLayout;
 import java.awt.image.BufferedImage;
+import java.awt.image.ColorModel;
+import java.awt.image.WritableRaster;
 
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
@@ -17,6 +19,13 @@ public class Images {
 		frame.pack();
 		frame.setVisible(true);
 		frame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+	}
+
+	public static BufferedImage copy(BufferedImage img) {
+		ColorModel cm = img.getColorModel();
+		boolean isAlphaPremulti = cm.isAlphaPremultiplied();
+		WritableRaster wr = img.getRaster().createCompatibleWritableRaster();
+		return new BufferedImage(cm, img.copyData(wr), isAlphaPremulti, null);
 	}
 
 	private Images() {
