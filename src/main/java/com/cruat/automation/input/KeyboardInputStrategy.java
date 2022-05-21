1391534ea6079d9bbded1dce0c1e91f20b8a74a9
Joseph <aelphaeis@live.com>
2022-05-20 21:30:07 -0400

    Default for keyboard input strategy

--- src/main/java/com/cruat/automation/input/KeyboardInputStrategy.java
+++ src/main/java/com/cruat/automation/input/KeyboardInputStrategy.java
@@ -2,5 +2,9 @@ package com.cruat.automation.input;
 
 public interface KeyboardInputStrategy {
 
+	public default void sendKey(int key) {
+		sendKey(key, 200);
+	}
+
 	public void sendKey(int key, int delay);
 }
