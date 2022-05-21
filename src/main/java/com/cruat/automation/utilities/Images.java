df17883b00919632a6eb35921fafb255e1fb1852
Joseph <aelphaeis@live.com>
2022-05-20 22:47:46 -0400

    Utility to write images to file

--- src/main/java/com/cruat/automation/utilities/Images.java
+++ src/main/java/com/cruat/automation/utilities/Images.java
@@ -3,14 +3,22 @@ package com.cruat.automation.utilities;
 import java.awt.FlowLayout;
 import java.awt.image.BufferedImage;
 import java.awt.image.ColorModel;
+import java.awt.image.RenderedImage;
 import java.awt.image.WritableRaster;
+import java.io.File;
+import java.io.IOException;
 
+import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.WindowConstants;
 
+import com.cruat.automation.AutomationException;
+
 public class Images {
+	
+	static final String PNG = "png";
 
 	public static void show(BufferedImage image) {
 		JFrame frame = new JFrame();
@@ -26,6 +34,16 @@ public class Images {
 		boolean isAlphaPremulti = cm.isAlphaPremultiplied();
 		WritableRaster wr = img.getRaster().createCompatibleWritableRaster();
 		return new BufferedImage(cm, img.copyData(wr), isAlphaPremulti, null);
+	}
+	
+
+	public static void toFile(RenderedImage image, File output) {
+		try {
+			ImageIO.write(image, Images.PNG, output);
+		}
+		catch (IOException e) {
+			throw new AutomationException(e);
+		}
 	}
 
 	private Images() {
