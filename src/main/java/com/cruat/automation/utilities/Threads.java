21049e469ddca6f1602becc1a4e112df34c00e2f
Joseph <aelphaeis@live.com>
2023-04-20 18:07:29 -0400

    Fail early if milliseconds specified is 0

--- src/main/java/com/cruat/automation/utilities/Threads.java
+++ src/main/java/com/cruat/automation/utilities/Threads.java
@@ -1,17 +1,19 @@
 package com.cruat.automation.utilities;
 
 public class Threads {
-	
+
 	public static void sleep(long milliseconds) {
 		pause(milliseconds);
 	}
 
 	public static void pause(long milliseconds) {
-		try {
-			Thread.sleep(milliseconds);
-		}
-		catch (InterruptedException e) {
-			Thread.currentThread().interrupt();
+		if (milliseconds != 0) {
+			try {
+				Thread.sleep(milliseconds);
+			}
+			catch (InterruptedException e) {
+				Thread.currentThread().interrupt();
+			}
 		}
 	}
 
