e1ca46d5da85f99adc9aa0bac73fe5321f837d7a
Joseph <aelphaeis@live.com>
2023-06-02 11:51:59 -0400

    Grayscale method

--- src/main/java/com/cruat/automation/utilities/Images.java
+++ src/main/java/com/cruat/automation/utilities/Images.java
@@ -18,7 +18,9 @@ import com.cruat.automation.AutomationException;
 
 public class Images {
 
-	static final String PNG = "png";
+	public static final String PREFIX = "tempMatch";
+	public static final String SUFFIX = ".png";
+	public static final String PNG = "png";
 
 	public static void show(BufferedImage image) {
 		JFrame frame = new JFrame();
@@ -27,6 +29,34 @@ public class Images {
 		frame.pack();
 		frame.setVisible(true);
 		frame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+	}
+
+	public static BufferedImage grayScale(BufferedImage source) {
+		BufferedImage destination = copy(source);
+		for (int y = 0; y < source.getHeight(); y++) {
+			for (int x = 0; x < source.getWidth(); x++) {
+				int rbg = source.getRGB(x, y);
+
+				byte alpha = (byte) ((rbg >> 24) & 0xFF);
+				byte red = (byte) ((rbg >> 16) & 0xFF);
+				byte green = (byte) ((rbg >> 8) & 0xFF);
+				byte blue = (byte) ((rbg >> 0) & 0xFF);
+
+				double rr = Math.pow(red / 255.0, 2.2);
+				double gg = Math.pow(green / 255.0, 2.2);
+				double bb = Math.pow(blue / 255.0, 2.2);
+
+				// Calculate luminance:
+				double lum = 0.2126 * rr + 0.7152 * gg + 0.0722 * bb;
+
+				// Gamma compand and rescale to byte range:
+				int val = (int) (255.0 * Math.pow(lum, 1.0 / 2.2));
+				int result = (alpha << 24) + (val << 16) + (val << 8) + val;
+
+				destination.setRGB(x, y, result);
+			}
+		}
+		return destination;
 	}
 
 	public static BufferedImage copy(BufferedImage img) {
