e4139b421c62b12433e2fafd199f9ea8be283450
Joseph <aelphaeis@live.com>
2024-03-05 19:38:56 -0500

    Allow conversions of Decisions to Futures

--- src/main/java/com/cruat/automation/capture/cv/ImagePredicate.java
+++ src/main/java/com/cruat/automation/capture/cv/ImagePredicate.java
@@ -1,8 +1,13 @@
 package com.cruat.automation.capture.cv;
 
 import java.awt.image.BufferedImage;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Future;
 import java.util.function.Predicate;
 
 public interface ImagePredicate extends Predicate<BufferedImage> {
 
+	default Future<Boolean> toFuture(BufferedImage image) {
+		return CompletableFuture.supplyAsync(() -> this.test(image));
+	}
 }
