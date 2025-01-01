dc13064bff675adea58b39ef04dfd505f89bb324
Joseph <aelphaeis@live.com>
2024-12-31 23:25:30 -0500

    Change modifiers in Diagnostics

--- src/main/java/com/cruat/automation/utilities/Diagnostics.java
+++ src/main/java/com/cruat/automation/utilities/Diagnostics.java
@@ -8,8 +8,8 @@ import java.time.format.DateTimeFormatter;
 public class Diagnostics {
 
 	public static final String DATETIME_FORMAT = "uuuu-MM-dd'T'HH-mm-ss-SSS";
-	private static final DateTimeFormatter DTF = defaultDateTimeFormatter();
-	private static final String BASE_DIR = "logs/";
+	public static final DateTimeFormatter DTF = defaultDateTimeFormatter();
+	public static final String BASE_DIR = "logs/";
 	private static final String PREFIX = "file://";
 
 	public static String logImage(RenderedImage ri) {
